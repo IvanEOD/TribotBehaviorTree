b35f8e5426d955fd1dc075e4392a762fcdba6728
IvanEOD <77090686+IvanEOD@users.noreply.github.com>
2022-09-18 07:47:53 -0400

    Update ScriptContext.java

--- ScriptContext.java
+++ ScriptContext.java
@@ -1,7 +1,31 @@
 package scripts.framework;
 
+@Data
 public class ScriptContext {
+    
+    private final String scriptName;
+    private final LocalDateTime startTime;
+    private final AtomicBoolean quitting = new AtomicBoolean(false);
+    private final ConcurrentHashMap<String, Object> variables;
 
+    public ScriptContext(String scriptName) {
+        this.scriptName = scriptName;
+        startTime = LocalDateTime.now();
+        variables = new ConcurrentHashMap<>();
+        Globals.get().setScriptName(scriptName);
+        Globals.get().setStartTime(startTime);
+    }
 
+    public void setVariable(String key, Object value) { variables.put(key, value); }
+    public <VariableType> VariableType getVariable(String key, VariableType defaultValue) { return (VariableType) variables.getOrDefault(key, defaultValue); }
+    public <VariableType> VariableType getVariable(String key) { if (variables.containsKey(key)) return (VariableType) variables.get(key); else return null; }
+    public boolean variableExists(String key) { return variables.containsKey(key); }
 
+    public void setQuitting(boolean value) { quitting.set(value); }
+    public boolean isQuitting() { return quitting.get(); }
+    
+    
+    
+    
+    
 }
